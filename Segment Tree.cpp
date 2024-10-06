ef8c0b0910106d85400a9180e880098b50d061ad
naitik-solanki <77410528+naitik-solanki@users.noreply.github.com>
2024-10-06 11:35:35 +0530

    Update Segment Tree.cpp

--- Segment Tree.cpp
+++ Segment Tree.cpp
@@ -1,146 +1,58 @@
-
-struct dat
-{
-	//Use required attributes
-	int mn;
-
-	//Default Values
-	dat() : mn(1e9) {};
+struct node {
+	ll mn = INF, mx = -INF, val = 0;
+	node() : mn(INF), mx(-INF), val(0) {};
 };
-
-struct SegTree
-{
-	int N;
-	vector<dat> st;
+struct SegTree {
+	ll N;
+	vector<node> st;
 	vector<bool> cLazy;
-	vector<int> lazy;
-
-	void init(int n)
-	{
+	vector<ll> lazy;
+	void init(ll n) {
 		N = n;
-		st=vector<dat>(4 * N + 5);
-		cLazy=vector<bool>(4 * N + 5, false);
-		lazy=vector<int>(4 * N + 5, 0);
+		st.resize(4 * N + 5);
+		cLazy.assign(4 * N + 5, false);
+		lazy.assign(4 * N + 5, 0);
 	}
-
-	//Write reqd merge functions
-	void merge(dat &cur, dat &l, dat &r) 
-	{
-		cur.mn = min(l.mn, r.mn);
+	void merge(node &cur, node &l, node &r) {
+		cur.mn = min(l.mn, r.mn), cur.mx = max(l.mx, r.mx), cur.val = l.val + r.val; // this is the normal function which you want to do for parents using left and right child
 	}
-	
-	//	Handle lazy propagation appriopriately
-	// 	think what should happen if clazy[2*node] is already 1 before propagate is called
-	void propagate(int node, int L, int R)
-	{
-		if(L != R)
-		{
-			cLazy[node*2] = 1;
-			cLazy[node*2 + 1] = 1;
-			lazy[node*2] = lazy[node];
-			lazy[node*2 + 1] = lazy[node]; 
-		}
-		st[node].mn = lazy[node];
-		cLazy[node] = 0;
+	void propagate(ll ver, ll L, ll R) {
+		if(L != R) {
+			cLazy[ver * 2] = cLazy[ver * 2 + 1] = 1;
+			lazy[ver * 2] += lazy[ver], lazy[ver * 2 + 1] += lazy[ver]; 
+		}   
+		st[ver].mn += lazy[ver], st[ver].mx += lazy[ver], st[ver].val += (R - L + 1) * lazy[ver]; // during lazy propagation you have to chg this line according to the condition
+		cLazy[ver] = lazy[ver] = 0;
 	}
-
-	void build(int node, int L, int R)
-	{
-		if(L==R)
-		{
-			st[node].mn = 1e9;
+	void build(ll ver, ll L, ll R, vector<ll> &a) {
+		if(L > R) return;
+		if(L == R) {
+			st[ver].mn = a[L - 1], st[ver].mx = a[L - 1], st[ver].val = a[L - 1];
 			return;
 		}
-		int M=(L + R)/2;
-		build(node*2, L, M);
-		build(node*2 + 1, M + 1, R);
-		merge(st[node], st[node*2], st[node*2+1]);
+		ll M = (L + R) / 2;
+		build(ver * 2, L, M, a), build(ver * 2 + 1, M + 1, R, a);
+		merge(st[ver], st[ver * 2], st[ver * 2 + 1]);
 	}
-
-	dat Query(int node, int L, int R, int i, int j)
-	{
-		if(cLazy[node])
-			propagate(node, L, R);
-		if(j<L || i>R)
-			return dat();
-		if(i<=L && R<=j)
-			return st[node];
-		int M = (L + R)/2;
-		dat left=Query(node*2, L, M, i, j);
-		dat right=Query(node*2 + 1, M + 1, R, i, j);
-		dat cur;
+	node Query(ll ver, ll L, ll R, ll i, ll j) {
+		if(cLazy[ver]) propagate(ver, L, R);
+		if(j < L || i > R) return node();
+		if(i <= L && R <= j) return st[ver];
+		ll M = (L+R) / 2;
+		node left = Query(ver * 2, L, M, i, j), right = Query(ver * 2 + 1, M + 1, R, i, j), cur;
 		merge(cur, left, right);
 		return cur;
 	}
-
-	dat pQuery(int node, int L, int R, int pos)
-	{
-		if(cLazy[node])
-			propagate(node, L, R);
-		if(L == R)
-			return st[node];
-		int M = (L + R)/2;
-		if(pos <= M)
-			return pQuery(node*2, L, M, pos);
-		else
-			return pQuery(node*2 + 1, M + 1, R, pos);
-	}	
-
-	void Update(int node, int L, int R, int i, int j, int val)
-	{
-		if(cLazy[node])
-			propagate(node, L, R);
-		if(j<L || i>R)
-			return;
-		if(i<=L && R<=j)
-		{
-			cLazy[node] = 1;
-			lazy[node] = val;
-			propagate(node, L, R);
+	void Update(ll ver, ll L, ll R, ll i, ll j, ll val) {
+		if(cLazy[ver]) propagate(ver, L, R);
+		if(j < L || i > R) return;
+		if(i <= L && R <= j) {
+			cLazy[ver] = 1, lazy[ver] += val;
+			propagate(ver, L, R);
 			return;
 		}
-		int M = (L + R)/2;
-		Update(node*2, L, M, i, j, val);
-		Update(node*2 + 1, M + 1, R, i, j, val);
-		merge(st[node], st[node*2], st[node*2 + 1]);
-	}
-
-	void pUpdate(int node, int L, int R, int pos, int val)
-	{
-		if(cLazy[node])
-			propagate(node, L, R);
-		if(L == R)
-		{
-			change_here
-			return;
-		}
-		int M = (L + R)/2;
-		if(pos <= M)
-			pUpdate(node*2, L, M, pos, val);
-		else
-			pUpdate(node*2 + 1, M + 1, R, pos, val);
-		dat tleft=Query(2*node,L,M,L,M);
-		dat tright=Query(2*node+1,M+1,R,M+1,R);
-		merge(st[node],tleft,tright);
-	}
-
-	dat query(int pos)
-	{
-		return pQuery(1, 1, N, pos);
-	}
-
-	dat query(int l, int r)
-	{
-		return Query(1, 1, N, l, r);
-	}
-
-	void update(int pos, int val)
-	{
-		pUpdate(1, 1, N, pos, val);
-	}
-
-	void update(int l, int r, int val)
-	{
-		Update(1, 1, N, l, r, val);
+		ll M = (L + R) / 2;
+		Update(ver * 2, L, M, i, j, val), Update(ver * 2 + 1, M + 1, R, i, j, val);
+		merge(st[ver], st[ver * 2],st[ver * 2 + 1]);
 	}
 };
